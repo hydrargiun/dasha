956bd254847898a60205ea5839b8045409179699
hydrargiun <54144611+hydrargiun@users.noreply.github.com>
2020-12-09 00:53:44 +0300

    Add files via upload

--- prog.cpp
+++ prog.cpp
@@ -7,25 +7,23 @@ int main(){
     string str;
     string a = "1";
     string b = "2";
-    for (int i =0;i<2018; i++) {
+    for (int i =0;i<198; i++) {
         str +=a;
     }
-    for (int i =0;i<2019; i++) {
-        str +=b;
-    }
+
     int rc = 0;
     do {
         rc = 0;
         for (int i = 0; i < str.size(); i++) {
-            if (str[i] == '1' && str[i + 1] == '1' && str[i + 2] == '1') {
-                str.replace(i, 3, 1, '2');
+            if (str[i] == '1' && str[i + 1] == '1' && str[i + 2] == '1' && str[i + 3] == '1') {
+                str.replace(i, 4, 2, '3');
                 rc = 1;
                 break;
             }
         }
         if(rc == 1) {
             for (int i = 0; i < str.size(); i++) {
-                if (str[i] == '2' && str[i + 1] == '2' && str[i + 2] == '2') {
+                if (str[i] == '3' && str[i + 1] == '3' && str[i + 2] == '3') {
                     str.replace(i, 3, 1, '1');
                     break;
                 }
